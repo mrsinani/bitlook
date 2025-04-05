3b384d4a11fcd1fa32f327e154627dea352c6d41
Danaid Sinani <danaid@bu.edu>
2025-04-04 23:38:33 -0400

    api endpoints working

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Health endpoint
-app.get("/health", (req, res) => {
+app.get("/api/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
@@ -18,6 +18,21 @@ app.get("/health", (req, res) => {
 app.get("/api/bitcoin-price", async (req, res) => {
   try {
     const response = await axios.get("https://mempool.space/api/v1/prices");
+    const { time, USD } = response.data;
+
+    res.status(200).json({ time, price: USD });
+  } catch (error) {
+    console.error("Error fetching Bitcoin price:", error);
+    res.status(500).json({ error: "Failed to fetch Bitcoin price" });
+  }
+});
+
+// Bitcoin history endpoint
+app.get("/api/bitcoin-history", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://mempool.space/api/v1/historical-price?currency=USD&timestamp="
+    );
     const { time, USD } = response.data;
 
     res.status(200).json({ time, price: USD });
