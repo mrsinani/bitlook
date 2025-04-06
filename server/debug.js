b239a375837d570b308788a9d217a7235ba2b3ed
Kevin Shan <kevinshan2017@gmail.com>
2025-04-05 22:31:55 -0400

    fox me

--- server/debug.js
+++ server/debug.js
@@ -1,6 +1,11 @@
-const axios = require("axios");
-const dotenv = require("dotenv");
-const path = require("path");
+import axios from "axios";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get current file path and directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Load environment variables from root .env file
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
--- server/proxy.js
+++ server/proxy.js
@@ -6,7 +6,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 
-// Get current file directory in ES modules
+// Get current file path and directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
